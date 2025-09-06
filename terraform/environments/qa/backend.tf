8fffa274d0ed161be64b9aef5e56c85d2d6f97e9
root123 <venkatasudheerkumar.kondeti@gmail.com>
2025-09-06 04:54:51 -0400

    Update backend configuration in QA environment with specific resource values

--- terraform/environments/qa/backend.tf
+++ terraform/environments/qa/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
+    resource_group_name  = "ecommerce-qa-rg"
+    storage_account_name = "qatfstateaccount"
+    container_name       = "qatfstate"
     key                  = "qa.terraform.tfstate"
   }
 }
