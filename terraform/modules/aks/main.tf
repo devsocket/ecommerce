efc229c8e8dec351846c9ceb9c1d6daae820e564
root123 <venkatasudheerkumar.kondeti@gmail.com>
2025-09-06 08:49:09 -0400

    Add GHCR authentication variables and update AKS module for image pull secret

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -2,17 +2,47 @@ resource "azurerm_kubernetes_cluster" "this" {
   name                = var.cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  dns_prefix         = "${var.cluster_name}-dns"
+  dns_prefix          = "${var.cluster_name}-dns"
 
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2ms"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = var.tags  
+  tags = var.tags
+}
+
+# Kubernetes provider using AKS kube_config
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
+}
+
+# GHCR image pull secret
+resource "kubernetes_secret" "ghcr_pull" {
+  metadata {
+    name      = "ghcr-secret"
+    namespace = "default"
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = base64encode(jsonencode({
+      auths = {
+        "ghcr.io" = {
+          username = var.ghcr_username
+          password = var.ghcr_pat
+          email    = "you@example.com"
+        }
+      }
+    }))
+  }
 }
--- terraform/modules/aks/variables.tf
+++ terraform/modules/aks/variables.tf
@@ -9,3 +9,14 @@ variable "tags" {
   type    = map(string)
   default = {}
 }
+variable "ghcr_username" {
+  description = "GitHub username for GHCR authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "ghcr_pat" {
+  description = "GitHub Personal Access Token for GHCR"
+  type        = string
+  sensitive   = true
+}
